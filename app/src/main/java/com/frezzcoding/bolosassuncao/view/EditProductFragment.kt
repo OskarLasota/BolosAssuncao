3f8de1f86b674240263ad54eb13726c5580b0ead
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-04-25 13:41:04 +0100

    setting up new fragment for editing products

---
+++ app/src/main/java/com/frezzcoding/bolosassuncao/view/EditProductFragment.kt
@@ -0,0 +1,22 @@
+package com.frezzcoding.bolosassuncao.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.frezzcoding.bolosassuncao.R
+
+class EditProductFragment : Fragment() {
+
+    private lateinit var _view : View
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _view =  inflater.inflate(R.layout.fragment_editproduct, container, false)
+
+
+        return _view
+    }
+
+
+}
