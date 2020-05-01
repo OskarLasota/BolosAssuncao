d9f86c8e9d1ab66af89f2dbc2bf6d87d63ed2c89
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-05-01 21:19:57 +0100

    setting up firebase and sending token to the database

---
+++ app/src/main/java/com/frezzcoding/bolosassuncao/utils/FirebaseMessagingService.kt
@@ -0,0 +1,74 @@
+package com.frezzcoding.bolosassuncao.utils
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.util.Log
+import androidx.core.app.NotificationCompat
+import com.frezzcoding.bolosassuncao.R
+import com.frezzcoding.bolosassuncao.view.MainActivity
+import com.google.firebase.messaging.RemoteMessage
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+
+class FirebaseMessagingService : com.google.firebase.messaging.FirebaseMessagingService() {
+
+
+    override fun onNewToken(token: String) {
+        //Log.e("new token", token)
+        //CoroutineScope(IO).launch {
+            storeToken(token)
+        //}
+        super.onNewToken(token)
+    }
+    private fun storeToken(token : String) {
+
+        if (token.isNotEmpty()) {
+            var client = OkHttpClient()
+            var body = FormBody.Builder()
+                .add("fcm_token", token)
+                .build()
+
+            var request = Request.Builder()
+                .url("http://www.bolosassuncao.com.br/get_notification.php")
+                .post(body)
+                .build()
+            try {
+                client.newCall(request).execute()
+            } catch (e: IOException) {
+
+            }
+        }
+    }
+
+
+    @Override
+    override fun onMessageReceived(message: RemoteMessage) {
+        showNotification(message.data["message"])
+    }
+
+    private fun showNotification(message : String?){
+        var intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+
+        var pending = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        var builder = NotificationCompat.Builder(this)
+            .setAutoCancel(true)
+            .setContentTitle("FCM Test")
+            .setContentText(message)
+            .setSmallIcon(R.drawable.common_google_signin_btn_icon_dark)
+            .setContentIntent(pending)
+
+        var manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        manager.notify(0,builder.build())
+    }
+
+}
--- app/src/main/java/com/frezzcoding/bolosassuncao/view/MainActivity.kt
+++ app/src/main/java/com/frezzcoding/bolosassuncao/view/MainActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.ui.NavigationUI
 import com.frezzcoding.bolosassuncao.R
 import kotlinx.android.synthetic.main.activity_main.*
-import com.frezzcoding.bolosassuncao.viewmodel.ProductViewModel
-import com.google.android.gms.tasks.OnCompleteListener
 import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+import okhttp3.FormBody
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
 
 
 class MainActivity : AppCompatActivity() {
@@ -27,30 +29,17 @@ class MainActivity : AppCompatActivity() {
         val navController = Navigation.findNavController(this,
             R.id.nav_host_fragment
         )
+        //check if this line below works
+        startService(this.intent)
 
         setupBottomNavMenu(navController)
         setupSideNavigationMenu(navController)
         setupActionBar(navController)
-        getFirebaseId()
+
+        FirebaseMessaging.getInstance().subscribeToTopic("test")
 
     }
 
-
-    private fun getFirebaseId(){
-        FirebaseInstanceId.getInstance().instanceId
-            .addOnCompleteListener(OnCompleteListener { task ->
-                if (!task.isSuccessful) {
-                    Log.w("tag1", "getInstanceId failed", task.exception)
-                    return@OnCompleteListener
-                }
-                // Get new Instance ID token
-                val token = task.result?.token
-
-                // Log and toast
-                val msg = getString(R.string.msg_token_fmt, token)
-                println("msg is : $msg")
-            })
-    }
 
     private fun setupBottomNavMenu(navController: NavController) {
         bottom_nav?.let {
