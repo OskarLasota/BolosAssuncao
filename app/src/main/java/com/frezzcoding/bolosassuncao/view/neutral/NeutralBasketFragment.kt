297890f702031eff8e95943c8216aa26e061454c
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-05-13 20:33:03 +0100

    chore: setting up basket fragment

--- app/src/main/java/com/frezzcoding/bolosassuncao/view/neutral/NeutralBasketFragment.kt
+++ app/src/main/java/com/frezzcoding/bolosassuncao/view/neutral/NeutralBasketFragment.kt
@@ -4,14 +4,48 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.frezzcoding.bolosassuncao.R
+import com.frezzcoding.bolosassuncao.adapters.BasketViewAdapter
+import com.frezzcoding.bolosassuncao.databinding.FragmentBasketBinding
+import com.frezzcoding.bolosassuncao.models.Product
+import com.frezzcoding.bolosassuncao.viewmodel.BasketViewModel
 
 class NeutralBasketFragment : Fragment() {
 
+    private lateinit var viewModel : BasketViewModel
+    private lateinit var adapterProduct : BasketViewAdapter
+    private var productList : ArrayList<Product> = ArrayList()
+    private lateinit var binding : FragmentBasketBinding
+
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_basket, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_basket, container, false
+        )
+
+        initializeViewModel()
+
+        return binding.root
     }
+
+
+    private fun initializeViewModel(){
+        viewModel = ViewModelProvider.AndroidViewModelFactory(activity!!.application).create(BasketViewModel(activity!!.application).javaClass)
+        viewModel.init()
+        viewModel.loading.observe(viewLifecycleOwner, observeLoading)
+    }
+
+    private val observeLoading = Observer<Boolean>{
+        if(it){
+            println("loading")
+        }else{
+            println("loading complete")
+        }
+    }
+
 
 }
