4a232f515264ee21aa4319e14956b394fd524a6c
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-05-20 18:04:52 +0100

    chore: creating layout for timetable picker and listeners

--- app/src/main/java/com/frezzcoding/bolosassuncao/view/privileged/PrivilegedTimetableFragment.kt
+++ app/src/main/java/com/frezzcoding/bolosassuncao/view/privileged/PrivilegedTimetableFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toDrawable
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.frezzcoding.bolosassuncao.R
@@ -12,6 +13,14 @@ import com.frezzcoding.bolosassuncao.databinding.FragmentTimetableBinding
 class PrivilegedTimetableFragment : Fragment() {
 
     private lateinit var binding : FragmentTimetableBinding
+    private var monPressed = false
+    private var tuePressed = false
+    private var wedPressed = false
+    private var thuPressed = false
+    private var friPressed = false
+    private var satPressed = false
+    private var sunPressed = false
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = DataBindingUtil.inflate(
@@ -20,9 +29,80 @@ class PrivilegedTimetableFragment : Fragment() {
 
         //initializeViewModel()
 
+        setListeners()
 
 
         return binding.root
+    }
+
+    private fun setListeners(){
+        //state of date selected should be stored on viewmodel and result should be sent to the database
+        binding.tvMonday.setOnClickListener {
+            if(monPressed) {
+                binding.tvMonday.background = null
+                monPressed = false
+            }else {
+                binding.tvMonday.setBackgroundResource(R.drawable.ic_dayselected)
+                monPressed = true
+            }
+        }
+        binding.tvTuesday.setOnClickListener {
+            if(tuePressed) {
+                binding.tvTuesday.background = null
+                tuePressed = false
+            }else {
+                binding.tvTuesday.setBackgroundResource(R.drawable.ic_dayselected)
+                tuePressed = true
+            }
+        }
+
+        binding.tvWednesday.setOnClickListener {
+            if(wedPressed) {
+                binding.tvWednesday.background = null
+                wedPressed = false
+            }else {
+                binding.tvWednesday.setBackgroundResource(R.drawable.ic_dayselected)
+                wedPressed = true
+            }
+        }
+        binding.tvThursday.setOnClickListener {
+            if(thuPressed) {
+                binding.tvThursday.background = null
+                thuPressed = false
+            }else {
+                binding.tvThursday.setBackgroundResource(R.drawable.ic_dayselected)
+                thuPressed = true
+            }
+        }
+        binding.tvFriday.setOnClickListener {
+            if(friPressed) {
+                binding.tvFriday.background = null
+                friPressed = false
+            }else {
+                binding.tvFriday.setBackgroundResource(R.drawable.ic_dayselected)
+                friPressed = true
+            }
+        }
+        binding.tvSaturday.setOnClickListener {
+            if(satPressed) {
+                binding.tvSaturday.background = null
+                satPressed = false
+            }else {
+                binding.tvSaturday.setBackgroundResource(R.drawable.ic_dayselected)
+                satPressed = true
+            }
+        }
+        binding.tvSunday.setOnClickListener {
+            if(sunPressed) {
+                binding.tvSunday.background = null
+                sunPressed = false
+            }else {
+                binding.tvSunday.setBackgroundResource(R.drawable.ic_dayselected)
+                sunPressed = true
+            }
+        }
+
+
     }
 
 }
