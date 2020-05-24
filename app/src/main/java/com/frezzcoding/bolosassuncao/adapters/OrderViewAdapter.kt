ebc81309bd517843370c00b105de4326975c6e9d
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-05-24 18:15:21 +0100

    chore: set up order view for current user

--- app/src/main/java/com/frezzcoding/bolosassuncao/adapters/OrderViewAdapter.kt
+++ app/src/main/java/com/frezzcoding/bolosassuncao/adapters/OrderViewAdapter.kt
@@ -1,4 +1,55 @@
 package com.frezzcoding.bolosassuncao.adapters
 
-class OrderViewAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.frezzcoding.bolosassuncao.R
+import com.frezzcoding.bolosassuncao.databinding.OrderCardviewBinding
+import com.frezzcoding.bolosassuncao.models.Order
+import com.frezzcoding.bolosassuncao.viewmodel.OrderViewModel
+
+class OrderViewAdapter(private val _data : List<Order>, private val viewModel : OrderViewModel) : RecyclerView.Adapter<OrderViewAdapter.ViewHolder>(){
+
+    private lateinit var binding : OrderCardviewBinding
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        var inflater : LayoutInflater = LayoutInflater.from(parent.context)
+        binding = DataBindingUtil.inflate(inflater, R.layout.order_cardview, parent, false)
+
+        return ViewHolder(binding, viewModel)
+    }
+
+    override fun getItemCount(): Int {
+        return _data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val order : Order = _data[position]
+        holder.bind(order)
+    }
+
+
+
+    class ViewHolder(private val binding: OrderCardviewBinding, private val viewModel : OrderViewModel) : RecyclerView.ViewHolder(binding.root) {
+        private lateinit var order : Order
+
+        init{
+            /*
+            binding.ivRemove.setOnClickListener {
+                viewModel.delete(order)
+            }
+
+             */
+        }
+
+        fun bind(_order : Order){
+            binding.order = _order
+            order = _order
+            //set image using picasso library, try to add this code in the model so we can databind
+            //Picasso.get().load(_order.url).into(binding.imageviewId)
+        }
+
+
+    }
+
 }
--- app/src/main/java/com/frezzcoding/bolosassuncao/view/neutral/NeutralOrdersFragment.kt
+++ app/src/main/java/com/frezzcoding/bolosassuncao/view/neutral/NeutralOrdersFragment.kt
@@ -2,16 +2,70 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.GridLayoutManager
 import com.frezzcoding.bolosassuncao.R
+import com.frezzcoding.bolosassuncao.adapters.OrderViewAdapter
+import com.frezzcoding.bolosassuncao.adapters.PrivOrderViewAdapter
+import com.frezzcoding.bolosassuncao.databinding.FragmentOrdersBinding
+import com.frezzcoding.bolosassuncao.di.OrderInjection
+import com.frezzcoding.bolosassuncao.models.Order
+import com.frezzcoding.bolosassuncao.models.User
+import com.frezzcoding.bolosassuncao.viewmodel.CachingViewModel
+import com.frezzcoding.bolosassuncao.viewmodel.OrderViewModel
 
 
 class NeutralOrdersFragment : Fragment() {
 
+    private lateinit var viewModel : OrderViewModel
+    private lateinit var cachingViewModel : CachingViewModel
+    private lateinit var binding : FragmentOrdersBinding
+    private lateinit var adapterProduct : OrderViewAdapter
+    private lateinit var currentUser : User
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_orders, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater, R.layout.fragment_orders, container, false
+        )
+
+        initializeViewModel()
+        viewModel.getOrders()
+
+        return binding.root
     }
 
+    private fun initializeViewModel(){
+        viewModel = ViewModelProvider(this, OrderInjection.provideViewModelFactory()).get(OrderViewModel::class.java)
+        viewModel.orders.observe(viewLifecycleOwner, renderOrders)
+
+        cachingViewModel = ViewModelProvider.AndroidViewModelFactory(activity!!.application).create(
+            CachingViewModel(activity!!.application).javaClass)
+        cachingViewModel.init()
+        cachingViewModel.user.observe(viewLifecycleOwner, getCachedUser)
+    }
+
+    private val getCachedUser = Observer<User>{
+        currentUser = it
+        viewModel.getOrders()
+    }
+
+    private val renderOrders = Observer<ArrayList<Order>>{
+        var list : List<Order> = mutableListOf()
+        for(element in it){
+            if(element.user_id == currentUser.id){
+                list = list.plus(element)
+            }
+        }
+        println(list.size)
+        ///productList = it
+        adapterProduct = OrderViewAdapter(list, viewModel)
+        binding.ordersRecycler.layoutManager = GridLayoutManager(this.requireContext(), 1)
+        binding.ordersRecycler.adapter = adapterProduct
+        //println(it[0].customer_name)
+        //make another call to get all products for that order
+    }
 
 }
