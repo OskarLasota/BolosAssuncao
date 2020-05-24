2edd7bc1a91b9ca8448a8e629eb0e5698e294904
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-05-24 12:14:29 +0100

    chore: setting up methods to upload the order to the server - needs to be coded better

--- app/src/main/java/com/frezzcoding/bolosassuncao/repo/OrderRepository.kt
+++ app/src/main/java/com/frezzcoding/bolosassuncao/repo/OrderRepository.kt
@@ -10,13 +10,51 @@ import com.frezzcoding.bolosassuncao.models.Product
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import kotlin.math.absoluteValue
 
 class OrderRepository : OrderDataSource {
     private var call: Call<ArrayList<Order>> ?= null
-    private var genericCall : Call<UploadResult>?= null
+    private var genericCall : Call<Int>?= null
 
-    override fun geneticOperation(operation: Int, Order: Order, callback: UploadCallBack<Boolean>) {
-        TODO("Not yet implemented")
+    private val OPERATION_UPLOAD = 1
+
+    override fun uploadOrderProducts(productId: Int, orderId: Int, callback: UploadCallBack<Boolean>) {
+
+    }
+
+
+    override fun geneticOperation(operation: Int, order: Order, callback: UploadCallBack<Int>) {
+        if(operation ==OPERATION_UPLOAD) {
+            genericCall = ApiClient.build()?.submit_order(
+                order.id,
+                order.user_id,
+                order.customer_name,
+                order.delivery_time,
+                order.delivery_date,
+                order.mobile,
+                order.address_1,
+                order.address_2,
+                order.postcode,
+                order.delivery_collection,
+                order.instructions,
+                order.payment_type,
+                order.status
+            )
+        }
+        genericCall?.enqueue(object: Callback<Int>{
+            override fun onFailure(call: Call<Int>, t: Throwable) {
+                callback.onError(t.message)
+            }
+
+            override fun onResponse(call: Call<Int>, response: Response<Int>) {
+                response?.body()?.let{
+                    if(response.isSuccessful){
+                        callback.onSuccess(it)
+                    }
+                }
+            }
+
+        })
     }
 
     override fun retrieveOrders(callback: OperationCallBack<Order>) {
