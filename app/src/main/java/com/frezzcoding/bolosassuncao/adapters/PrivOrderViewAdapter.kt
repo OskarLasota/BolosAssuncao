c98484a6418873248b22362a001257668af91e1e
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-06-13 19:03:39 +0100

    chore: setting up status changes for order

--- app/src/main/java/com/frezzcoding/bolosassuncao/adapters/PrivOrderViewAdapter.kt
+++ app/src/main/java/com/frezzcoding/bolosassuncao/adapters/PrivOrderViewAdapter.kt
@@ -1,24 +1,32 @@
 package com.frezzcoding.bolosassuncao.adapters
 
+import android.app.Dialog
+import android.content.Context
+import android.content.DialogInterface
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.frezzcoding.bolosassuncao.R
 import com.frezzcoding.bolosassuncao.databinding.PrivOrderCardviewBinding
 import com.frezzcoding.bolosassuncao.models.Order
 import com.frezzcoding.bolosassuncao.viewmodel.OrderViewModel
-import com.squareup.picasso.Picasso
 
 class PrivOrderViewAdapter(private val _data : List<Order>, private val viewModel : OrderViewModel) : RecyclerView.Adapter<PrivOrderViewAdapter.ViewHolder>(){
 
     private lateinit var binding : PrivOrderCardviewBinding
+    private lateinit var ctx : Context
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        var inflater : LayoutInflater = LayoutInflater.from(parent.context)
+        ctx = parent.context
+        var inflater : LayoutInflater = LayoutInflater.from(ctx)
         binding = DataBindingUtil.inflate(inflater, R.layout.priv_order_cardview, parent, false)
 
-        return ViewHolder(binding, viewModel)
+        return ViewHolder(binding, viewModel, ctx)
     }
 
     override fun getItemCount(): Int {
@@ -31,16 +39,28 @@ class PrivOrderViewAdapter(private val _data : List<Order>, private val viewMode
     }
 
 
-    class ViewHolder(private val binding: PrivOrderCardviewBinding, private val viewModel : OrderViewModel) : RecyclerView.ViewHolder(binding.root) {
+    class ViewHolder(private val binding: PrivOrderCardviewBinding, private val viewModel : OrderViewModel, private val ctx : Context) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
         private lateinit var order : Order
+        private var dialog = Dialog(ctx)
 
         init{
-            /*
-            binding.ivRemove.setOnClickListener {
-                viewModel.delete(order)
+
+            binding.ivStatus.setOnClickListener {
+                dialog.setContentView(R.layout.popup_select_status)
+                var btn1 = dialog.findViewById<Button>(R.id.btn1)
+                var btn2 = dialog.findViewById<Button>(R.id.btn2)
+                var btn3 = dialog.findViewById<Button>(R.id.btn3)
+                var btn4 = dialog.findViewById<Button>(R.id.btn4)
+                btn1.setOnClickListener { onClick(btn1) }
+                btn2.setOnClickListener { onClick(btn2) }
+                btn3.setOnClickListener { onClick(btn3) }
+                btn4.setOnClickListener { onClick(btn4) }
+
+                dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+                dialog.show()
             }
 
-             */
+
         }
 
         fun bind(_order : Order){
@@ -48,6 +68,16 @@ class PrivOrderViewAdapter(private val _data : List<Order>, private val viewMode
             order = _order
             //set image using picasso library, try to add this code in the model so we can databind
             //Picasso.get().load(_order.url).into(binding.imageviewId)
+        }
+
+        override fun onClick(v: View?) {
+            when(v?.id){
+                R.id.btn1 -> println("clicked btn1")
+                R.id.btn2 -> println("clicked btn2")
+                R.id.btn3 -> println("clicked btn3")
+                R.id.btn4 -> println("clicked btn4")
+            }
+            dialog.hide()
         }
 
 
