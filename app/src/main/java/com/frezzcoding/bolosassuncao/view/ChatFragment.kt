0dcf5fad0646d7d55b8d0d0314aa0c6ee47f5cea
Oskar Lasota <55768328+OskarLasota@users.noreply.github.com>
2020-06-14 14:05:35 +0100

    chore: intent to whatsapp messenger

--- app/src/main/java/com/frezzcoding/bolosassuncao/view/ChatFragment.kt
+++ app/src/main/java/com/frezzcoding/bolosassuncao/view/ChatFragment.kt
@@ -1,3 +1,5 @@
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +11,13 @@ import com.frezzcoding.bolosassuncao.R
 class ChatFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        // Inflate the layout for this fragment
+
+        var number = "+447773117678"
+        val url = "https://api.whatsapp.com/send?phone=$number"
+        val i = Intent(Intent.ACTION_VIEW)
+        i.data = Uri.parse(url)
+        startActivity(i)
+
         return inflater.inflate(R.layout.fragment_chat, container, false)
     }
 
